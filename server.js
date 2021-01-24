5d58ad25096cdf9cac96f5c1bf19ba9aa8924c71
hudsonmbarnes <hudsonmbarnes@gmail.com>
2021-01-23 16:36:37 -0800

    added api routes

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
+// Dependencies
 var express = require("express");
 var fs = require("fs");
+
+// Express app
 var app = express();
+
+// Port
 var PORT = process.env.PORT || 8080;
+
+// Data Parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Listener, starts server
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
