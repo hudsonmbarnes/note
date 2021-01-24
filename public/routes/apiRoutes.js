ac4f7568aed5b91bd91a1ca5633e5e23080df7a7
hudsonmbarnes <hudsonmbarnes@gmail.com>
2021-01-23 20:16:58 -0800

    started routing for notes

---
+++ public/routes/apiRoutes.js
@@ -0,0 +1,13 @@
+var fs = require("fs");
+
+// Api routing
+module.exports = function(app) {
+    
+    app.get("/api/notes", function(req, res) {
+       
+        res.json(data);
+
+    });
+
+
+}
